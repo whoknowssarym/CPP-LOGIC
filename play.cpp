2b1dbccd0a9321bef5aad8c610e7635dff152c19
Muhammad Sarim <154520178+whoknowssarym@users.noreply.github.com>
2024-08-23 23:27:46 +0500

    HALF PRACTICE OF PATTERNS USING NESTED LOOPS
    THIS CODE INCLUDES  
    1-> BOX PATTERN.
    2-> INCREASING STAR PATTERN.
    3-> DECREASING STAR PATTERN.
    4-> HALF PYRAMID PATTERN INCREASING.
    5-> HALF CHARACTER PYRAMID PATTERN INCREASING.
    <= More pattern will be uploaded tommorrow =>

---
+++ play.cpp
@@ -0,0 +1,78 @@
+// for box pattern
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int i = 1 ; i<=4  ; i++){   
+		 for(int j  = 1 ; j<= 4 ; j++){	
+		     cout<<i<<" ";
+		     //cout<<j<<" ";
+		}
+	 cout<<endl;
+	}
+    
+	return 0;
+}
+
+
+// for stars pattern increasing
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n = 5;
+	for(int i = 1 ; i<=n ; i++){
+		for(int j = 1 ; j<=i ; j++){
+			cout<<"*"<<" ";			
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+
+
+// for stars pattern decreasing (INverting)
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n = 1 ; 
+	for(int i = 10 ; i >= n ; i--){
+		for(int j = 1 ; j<=i ; j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+
+
+// for half pyramid pattern
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n = 5;
+	for(int i = 1 ; i<=n ; i++){
+		for(int j = 1 ; j<=i ; j++){
+			cout<<j<<" ";			
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
